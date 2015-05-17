f6c3a5f9a36c044b4b69273bddc826506f4f68c5
LegitMiller <jordan.kay@gmail.com>
2015-05-16 20:54:31 -0600

    changed string to text on note comments

--- db/migrate/20150304192846_create_notes.rb
+++ db/migrate/20150304192846_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
       t.string :title
-      t.string :comment
+      t.text :comment
       t.integer :user_id
       t.integer :record_id
 
